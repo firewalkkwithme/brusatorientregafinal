04616440e21de59bf603c51ffabccbd3885f61af
safari <brusatorif@gmail.com>
2022-10-13 19:37:05 -0300

    brusatori fetch

---
+++ js/stock.js
@@ -0,0 +1,11 @@
+const catalogoHombres = [
+    {id: 1, nombre: "Remera Rei", precio: 1200, img: "reimen.jpg", categoria: "Remeras", recienLlegado: true, sale: false},
+    {id: 2, nombre: "Remera Supercampeones", precio: 1200, img: "tsubasamen.jpg", categoria: "Remeras", recienLlegado: true, sale: false},
+    {id: 3, nombre: "Short Akatsuki", precio: 2000, img: "akatsukishorts.jpg", categoria: "Shorts", recienLlegado: false, sale: true},
+    {id: 4, nombre: "Short Cartoons", precio: 2000, img: "cartoonshorts.jpg",categoria: "Shorts", recienLlegado: false, sale: false},
+    {id: 5, nombre: "Medias Pokemon", precio: 600, img: "mediasponemon.png", categoria: "Accesorios", recienLlegado: true, sale: false},
+    {id: 6, nombre: "Buzo Dragon Ball", precio: 6000, img: "buzodragon.jpg", categoria: "Buzos", recienLlegado: true, sale: false},
+    {id: 7, nombre: "Remera Saint Seiya", precio: 1200, img: "saint.jpg", categoria: "Remeras", recienLlegado: true, sale: false},
+    {id: 8, nombre: "Zapatillas Levi", precio: 5000, img: "levi.jpg", categoria: "Accesorios", recienLlegado: true, sale: false},
+    
+];
